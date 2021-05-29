645902addb3c1daf80f99c0114849545e6ced5b9
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2021-05-29 17:59:38 -0300

    Com testes 2

--- src/components/TextInput/TextInput.spec.jsx
+++ src/components/TextInput/TextInput.spec.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { TextInput } from '.';
